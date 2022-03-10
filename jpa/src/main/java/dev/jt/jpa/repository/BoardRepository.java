1c9c56f9a2a120ee64d0e54e418dd5deca6738a9
LIM JT(임정택) <38105420+wjdxor@users.noreply.github.com>
2022-03-10 09:07:56 +0900

    Delete jpa/src/main directory

--- jpa/src/main/java/dev/jt/jpa/repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.hsooovn.jpa.repository;
-
-import dev.hsooovn.jpa.entity.BoardEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BoardRepository extends CrudRepository<BoardEntity, Long> {
-}
--- jpa/src/main/java/dev/jt/jpa/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.hsooovn.jpa.repository;
-
-import dev.hsooovn.jpa.entity.PostEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PostRepository extends CrudRepository<PostEntity, Long> {
-}
